3549c996e1adf7ef50573e4f9cb5c3bb3f27694f
alvaro <agomez@yepcode.io>
2025-07-31 16:51:53 +0200

    Obtain access token from yepcode api instead of keycloak

--- nodes/YepCode/transport/base64Utils.ts
+++ nodes/YepCode/transport/base64Utils.ts
@@ -1,39 +1,5 @@
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 
-export function base64Encode(str: string): string {
-	// Convert string to UTF-8 bytes
-	const bytes: number[] = [];
-	for (let i = 0; i < str.length; i++) {
-		let code = str.charCodeAt(i);
-		if (code < 0x80) {
-			bytes.push(code);
-		} else if (code < 0x800) {
-			bytes.push(0xc0 | (code >> 6));
-			bytes.push(0x80 | (code & 0x3f));
-		} else if (code < 0x10000) {
-			bytes.push(0xe0 | (code >> 12));
-			bytes.push(0x80 | ((code >> 6) & 0x3f));
-			bytes.push(0x80 | (code & 0x3f));
-		}
-	}
-
-	let encoded = '';
-	for (let i = 0; i < bytes.length; i += 3) {
-		const b1 = bytes[i];
-		const b2 = i + 1 < bytes.length ? bytes[i + 1] : 0;
-		const b3 = i + 2 < bytes.length ? bytes[i + 2] : 0;
-
-		const triplet = (b1 << 16) | (b2 << 8) | b3;
-
-		encoded += chars[(triplet >> 18) & 0x3f];
-		encoded += chars[(triplet >> 12) & 0x3f];
-		encoded += i + 1 < bytes.length ? chars[(triplet >> 6) & 0x3f] : '=';
-		encoded += i + 2 < bytes.length ? chars[triplet & 0x3f] : '=';
-	}
-
-	return encoded;
-}
-
 export function base64Decode(str: string): string {
 	// Remove any padding
 	str = str.replace(/=+$/, '');
